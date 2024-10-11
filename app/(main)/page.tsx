3540018554cb4c1bc7b50b0759ed7b7212d21a46
mxrain <mxrain@yeah.net>
2024-10-11 15:04:36 +0800

    add

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+
 const Page = () => {
   return <div>Page</div>;
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,4 +10,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
+
+export default config;
